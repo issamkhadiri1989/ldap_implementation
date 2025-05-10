980d90092e17405a039b364ac27205c358e4e6bf
Issam KHADIRI <khadiri.issam@gmail.com>
2025-05-10 23:57:23 +0100

    test ci cd

--- src/Security/Ldap/RoleHandler.php
+++ src/Security/Ldap/RoleHandler.php
@@ -6,6 +6,11 @@ namespace App\Security\Ldap;
 
 class RoleHandler
 {
+    /**
+     * Retrieves user's roles from the groups he belongs to.
+     *
+     * @return array
+     */
     public function findRoles(): array
     {
         return [];
